1c964e8814f4c116f5557a85b4c44a963f47ae79
Compass <ndphong165@gmail.com>
2022-08-18 23:33:06 +0700

    Practice test setMap and HashMap

---
+++ CollectionFramwork/Practice/src/phanbietHashMapvaHashSet/Main.java
@@ -0,0 +1,39 @@
+package phanbietHashMapvaHashSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Phong",23, "QN");
+        Student student2 = new Student("Hung",24, "YB");
+        Student student3 = new Student("Huy",23, "TB");
+        Student student4 = new Student("Luong",24, "BN");
+
+        Map<Integer, Student> studentMap = new HashMap<Integer, Student>();
+        studentMap.put(1,student1);
+        studentMap.put(2,student2);
+        studentMap.put(3,student3);
+        studentMap.put(4,student4);
+        studentMap.put(5,student1);
+        for (Map.Entry<Integer, Student> student: studentMap.entrySet())
+            System.out.println(student.toString());
+
+
+        System.out.println("...........Set");
+        Set<Student> students = new HashSet<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student1);
+
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+
+}
